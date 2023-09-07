c386d040acd2376b965712f83ae6e97fa29fcf4c
dev-dhan <roldhan.fulo.alcantara@gmail.com>
2023-09-07 10:19:07 +0800

    initial commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+hamburger = document.querySelector(".hamburger" );
+hamburger.onclick = function() { 
+    navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+}
+
+    /**Go Up Button */
+    $(document).ready(function(){
+
+        $(window).scroll(function(){
+          if($(this).scrollTop() > 40){
+            $('#topBtn').fadeIn();
+          } else{
+            $('#topBtn').fadeOut();
+          }
+        });
+      
+        $("#topBtn").click(function(){
+          $('html ,body').animate({scrollTop : 0},800);
+        });
+      });
